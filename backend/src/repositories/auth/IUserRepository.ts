499867ed927eabc239b1a68eb0ff8529201424e3
gabryel.souzz <gabryel.souza@arapiraca.ufal.br>
2026-05-10 18:55:20 -0300

    feat: improve userRepo to create users

--- backend/src/repositories/auth/IUserRepository.ts
+++ backend/src/repositories/auth/IUserRepository.ts
@@ -1,5 +1,14 @@
-import type { User } from "@prisma/client";
+import type { User, Student, Teacher, External } from "@prisma/client";
 
 export interface IUserRepository {
-  findByEmail(email: string): Promise<User | null>;
+
+  findByEmail(
+    email: string
+  ): Promise<User | null>;
+
+  create(data: Omit<User, "id" | "createdAt" | "updatedAt">): Promise<User>;
+
+  createStudent(data: Omit<Student, "createdAt" | "updatedAt">): Promise<Student>;
+  createTeacher(data: Omit<Teacher, "createdAt" | "updatedAt">): Promise<Teacher>;
+  createExternal(data: Omit<External, "createdAt" | "updatedAt">): Promise<External>
 }
--- backend/src/repositories/auth/UserRepository.ts
+++ backend/src/repositories/auth/UserRepository.ts
@@ -1,4 +1,4 @@
-import type { PrismaClient, User } from "@prisma/client";
+import type { PrismaClient, User, Student, Teacher, External } from "@prisma/client";
 import type { IUserRepository } from "@/repositories/auth/IUserRepository.js";
 import { prisma } from "@/database/prisma.js";
 
@@ -16,6 +16,31 @@ class UserRepository implements IUserRepository {
   public async findByEmail(email: string): Promise<User | null> {
     return this._prisma.user.findUnique({ where: { email } });
   }
+
+  public async create(
+    data: Omit<User, "id" | "createdAt" | "updatedAt">,
+  ): Promise<User> {
+    return this._prisma.user.create({ data });
+  }
+
+  public async createStudent(
+    data: Omit<Student, "createdAt" | "updatedAt">,
+  ): Promise<Student> {
+    return this._prisma.student.create({ data });
+  }
+
+  public async createTeacher(
+    data: Omit<Teacher, "createdAt" | "updatedAt">,
+  ): Promise<Teacher> {
+    return this._prisma.teacher.create({ data });
+  }
+
+  public async createExternal(
+    data: Omit<External, "createdAt" | "updatedAt">,
+  ): Promise<External> {
+    return this._prisma.external.create({ data });
+  }
+
 }
 
 export default UserRepository;
