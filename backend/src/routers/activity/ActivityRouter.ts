a778bb09680ae85f0e272524ffdaa55f56eaf9c6
gabryelsouzz <gabryel.souza@arapiraca.ufal.br>
2026-05-24 02:28:54 -0300

    feat: bring create activity route online

---
+++ backend/src/routers/activity/ActivityRouter.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import ActivityController from "@/controllers/activity/ActivityController.js";
+import AuthMiddleware from "@/middlewares/auth/AuthMiddleware.js";
+import type { IActivityController } from "@/controllers/activity/IActivityController.js";
+
+import type { IAuthMiddleware } from "@/middlewares/auth/IAuthMiddleware.js";
+
+
+const router = express.Router();
+const activityController: IActivityController = new ActivityController();
+
+const authMiddleware:     IAuthMiddleware      = new AuthMiddleware();
+
+router.post(
+  "/activity",
+  authMiddleware.auth(),
+  (req, res, next) => activityController.create(req, res).catch(next),
+);
+
+export default router;
