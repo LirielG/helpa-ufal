db3d2bb5badec3da4e3838cf34b0380a08cf94c7
gabryelsouzz <gabryel.souza@arapiraca.ufal.br>
2026-05-24 13:12:43 -0300

    fix: specify CORS and Api's URL

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -2,6 +2,6 @@ const isDevelopment = import.meta.env.MODE === "development";
 
 export const config = {
   apiUrl: isDevelopment
-    ? import.meta.env.VITE_API_URL || "http://localhost:3001/api"
+    ? import.meta.env.VITE_API_URL || "http://localhost:3333"
     : import.meta.env.VITE_API_URL || "https://api.helpa.com/api",
 };
