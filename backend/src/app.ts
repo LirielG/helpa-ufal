e029f8bd85cfc43df2cbe0516b61a336fb6f186d
gabryelsouzz <gabryel.souza@arapiraca.ufal.br>
2026-09-08 13:19:12 -0300

    feat: read CORS origin from CORS_ORIGIN

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,30 +3,39 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import Router from "@/routers/index.js";
 import errorHandler from "@/controllers/error/ErrorHandler.js";
+import { env } from "@/config/env.js";
+
 
 import swaggerUi from "swagger-ui-express";
 import { swaggerDocument } from "@/docs/swagger.js";
 
+
 const app = express();
+
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: env.CORS_ORIGIN,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
+
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use(Router);
 
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use(errorHandler.zodErrorHandler.bind(errorHandler));
 app.use(errorHandler.validationErrorHandler.bind(errorHandler));
 app.use(errorHandler.defaultHandler.bind(errorHandler));
+
 
 export { app };
