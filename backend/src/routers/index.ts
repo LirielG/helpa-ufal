b9085e92d26bc2c00933b7608e4de895222535b2
gabryelsouzz <gabryel.souza@arapiraca.ufal.br>
2026-09-15 00:05:40 -0300

    feat: register GET /activities/:activityId/enrollments

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -1,12 +1,15 @@
+// Substitui src/routers/index.ts
 import express from "express";
 import AuthRouter from "@/routers/auth/AuthRouter.js";
 import ActivityRouter from "@/routers/activity/ActivityRouter.js";
+import EnrollmentRouter from "@/routers/enrollment/EnrollmentRouter.js";
 import SigaaActivityRouter from "@/routers/sigaa/SigaaActivityRouter.js";
 
 const router = express.Router();
 
 router.use("/auth", AuthRouter);
 router.use("/", ActivityRouter);
+router.use("/", EnrollmentRouter);
 router.use("/", SigaaActivityRouter);
 
 export default router;
